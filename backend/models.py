f0f983714b2e33f57e154194e8819651d2086445
Raddames <raddamestonui48@gmail.com>
2024-07-08 21:31:17 +0300

    added migrate

--- backend/models.py
+++ backend/models.py
@@ -12,18 +12,15 @@ class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False, index=True) # index = True improves query performance on  fields.
-    email = db.Column(db.String(50), unique=True, nullable=False, index=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     profile_pictures = db.Column(db.String(300))
     is_admin = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.datetime, default=db.func.now())
-    updated_at = db.Column(db.datetime, default=db.func.now(), onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
     applications = db.relationship("Application", back_populates="user", cascade="all, delete-orphan")
-
-    def __repr__(self):
-        return f'<User {self.id}, {self.username}, {self.email}, {self.is_admin}>'
 
 class Job(db.Model, SerializerMixin):
     __tablename__ = "jobs"
@@ -39,23 +36,17 @@ class Job(db.Model, SerializerMixin):
     company = db.relationship("Company", back_populates="jobs")
     applications = db.relationship("Application", back_populates="job", cascade="all, delete-orphan")
 
-    def __repr__(self):
-        return f'<Job {self.id}, {self.title}>'
-    
 class Company(db.Model, SerializerMixin):
     __tablename__ = "companies"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False, index=True) 
+    name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
     location = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
     jobs = db.relationship("Job", back_populates="company")
-
-    def __repr__(self):
-        return f'<Company {self.id}, {self.name}>'
 
 # Association table to store many-to-many relationship between job and user
 class Application(db.Model, SerializerMixin):
@@ -71,6 +62,3 @@ class Application(db.Model, SerializerMixin):
 
     user = db.relationship("User", back_populates="applications")
     job = db.relationship("Job", back_populates="applications")
-
-    def __repr__(self):
-        return f'<Application {self.id}, {self.status}>'
