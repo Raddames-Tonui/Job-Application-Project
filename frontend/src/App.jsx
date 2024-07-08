cb1884a2fdb839e5502b25e325909b78b74c5818
Raddames <raddamestonui48@gmail.com>
2024-07-08 11:07:53 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline bg-slate-400">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
