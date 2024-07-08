8df0021dc90e378c69abbdf0ef5ac2c24d4ee446
Raddames <raddamestonui48@gmail.com>
2024-07-08 21:18:36 +0300

    added app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db, User, Job, Company , Application 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
 
 @app.route('/')
-def hello():
-    return 'Welcome to Job Search!'
+def home():
+    return "Welcome to the Job Portal API"
 
 
 if __name__ == "__main__":
