e15cee8682c962e0e54aece29c48d365bdd427fc
Raddames <raddamestonui48@gmail.com>
2024-07-09 18:02:41 +0300

    changed routes

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { ToastContainer } from 'react-toastify'
-
+import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from 'react-router-dom';
+
 
 function Layout() {
   return (
-    <div className='space-y-16'>
+    <div>
       <Navbar />
       <ToastContainer />
 
--- frontend/src/context/CompanyContext.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react'
-
-function CompanyContext() {
-  return (
-    <div>CompanyContext</div>
-  )
-}
-
-export default CompanyContext
