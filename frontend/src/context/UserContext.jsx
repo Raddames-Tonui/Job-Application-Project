e051297b4b0384047d726b957ce35c31e1607ce1
Raddames <raddamestonui48@gmail.com>
2024-07-12 08:08:25 +0300

    Resolve conflicts and apply stash@{0}

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,162 @@
+
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [onChange, setOnChange] = useState(false);
+  const [auth_token, setAuth_token] = useState(() => localStorage.getItem("access_token") || null);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // Register User
+  const register = (username, email, password) => {
+    fetch('http://localhost:5555/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, email, password })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.message === "User created successfully") {
+        navigate('/login');
+        toast.success("Registration successful. Please login.");
+      } else if (res.message === "User with this email or username already exists") {
+        toast.error("User with this email or username already exists.");
+      } else {
+        toast.error("Registration failed.");
+      }
+    });
+
+  };
+
+  // Login User
+  const login = (email, password) => {
+    fetch('http://localhost:5555/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.access_token) {
+        setAuth_token(res.access_token);
+        localStorage.setItem('access_token', res.access_token);
+
+        // Check if the user is an admin and navigate accordingly
+        if (res.is_admin) {
+          navigate('/admin');
+        } else {
+          navigate('/dashboard');
+        }
+
+        toast.success("Login successful.");
+      } else {
+        toast.error("Invalid username or password.");
+      }
+    });
+
+  };
+
+  // Update User
+  const update_user = (name, phone_number, is_organizer, password) => {
+    const server_url = 'http://localhost:5555'; // Define server_url if not already defined
+    fetch(`${server_url}/users`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: name,
+        password: password,
+        phone_number: phone_number,
+        is_organizer: is_organizer
+      }),
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      },
+    })
+    .then(response => response.json())
+    .then(res => {
+      if (res.success) {
+        toast.success(res.success);
+      } else if (res.error) {
+        toast.error(res.error);
+      } else {
+        toast.error("An error occurred.");
+      }
+    });
+
+  };
+
+// Logout User
+const logout = () => {
+  fetch('http://localhost:5555/logout', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    }
+  })
+  .then(response => response.json())
+  .then(res => {
+    if (res.success) {
+      localStorage.removeItem("access_token");
+      setCurrentUser(null);
+      setAuth_token(null);
+      toast.success(res.success);
+    } else if (res.error) {
+      toast.error(res.error);
+    }
+  });
+};
+
+useEffect(() => {
+  if (auth_token) {
+    fetch('http://localhost:5555/current_user', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.email) {
+        setCurrentUser(data);
+      } else {
+        localStorage.removeItem('access_token');
+        setAuth_token(null);
+        setCurrentUser(null);
+        navigate("/login");
+      }
+    });
+  } else {
+    // If auth_token is null, clear currentUser and navigate to login page
+    setCurrentUser(null);
+    navigate("/login");
+  }
+}, [auth_token, navigate]);
+
+  console.log(currentUser);
+  console.log(auth_token)
+  const contextData = {
+    auth_token,
+    currentUser,
+    setCurrentUser,
+    register,
+    login,
+    logout,
+    update_user
+  };
+
+  return (
+    <UserContext.Provider value={contextData}>
+      {children}
+    </UserContext.Provider>
+  );
+};
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,46 +1,97 @@
-import React, { useState } from 'react';
-const LandingPage = () => {
-  const [email, setEmail] = useState('');
+import React, { useState, useEffect, useContext } from "react";
+import backgroundImage from "./assets/cover_photo.jpg";
+import Footer from "./components/Footer";
+import FilteredJobs from "./components/FilteredJobs";
+import AvailableJobs from "./components/AvailableJobs";
+import { useNavigate } from "react-router-dom";
+// import { UserContext } from "../context/UserContext";
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Subscribe user to newsletter
-    console.log('Subscribing email:', email);
-    // Reset the form
-    setEmail('');
+function LandingPage() {
+
+  
+  const [jobs, setJobs] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/jobs')
+      .then(res => res.json())
+      .then(data => setJobs(data))
+      .catch(error => console.error('Error fetching jobs:', error));
+  }, []);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
   };
 
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="max-w-md w-full bg-white p-8 rounded shadow-lg">
-        <h1 className="text-3xl font-bold mb-4">Welcome to Job Portal</h1>
-        <p className="text-gray-600 mb-4">Explore and find your dream job today!</p>
+  const filteredJobs = jobs.filter((job) =>
+    job.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-        {/* Signup Form */}
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input
-              id="email"
-              type="email"
-              className="w-full border-gray-300 rounded px-4 py-2"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+  // Effect to toggle body overflow
+  useEffect(() => {
+    const handleScrollLock = () => {
+      document.body.style.overflow = isSearching ? "hidden" : "auto";
+    };
+
+    handleScrollLock();
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isSearching]);
+
+  return (
+    <div >
+      <div
+        className="bg-[#F3F4F6] pt-16 pb-32 px-4 sm:px-6 lg:pb-40 lg:px-8"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+        }}
+      >
+        <div className="relative bg-white bg-opacity-10 p-6  rounded-lg shadow-lg">
+          <h1 className="text-center text-4xl font-bold leading-10 text-gray-900">
+            There Is{" "}
+            <span className="text-blue-500">Definitely An Opportunity</span>{" "}
+            Here For You!
+          </h1>
+          <p className="text-center mt-4 text-lg text-gray-600">
+            Find Jobs, Employment & Career Opportunities
+          </p>
+          <div className="mt-10 flex justify-center">
+            <div className=" shadow-md rounded-lg p-4 flex">
+              <input
+                type="text"
+                className="p-2 w-[30vw] rounded-l-md border border-gray-300"
+                placeholder="Job title"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                onFocus={() => setIsSearching(true)}
+                onBlur={() => setIsSearching(false)}
+              />
+              <button className="p-2 bg-blue-500 text-white rounded-r-md">
+                Search
+              </button>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-            <input
-              id="password"
-              type="password"
-              className="w-full border-gray-300 rounded px-4 py-2"
-              placeholder="Enter your password"
-            />
-          </div>
-          <button type="submit" className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600 transition-colors duration-300">Sign Up</button>
-        </form>
+          {searchQuery && (
+            <div className=" flex justify-center">
+              <FilteredJobs filteredJobs={filteredJobs} />
+            </div>
+          )}
+        </div>
       </div>
+      <div>
+        
+      </div>
+      <AvailableJobs jobs={jobs} />
+      <Footer />
     </div>
   );
 }
